fc8560c892f7d396569c44a5e568e8dde6773809
Kishan Suresh <kishansureshin@gmail.com>
2021-09-10 02:07:41 +0100

    added html templates and finalized the error handling too

--- userapp/models.py
+++ userapp/models.py
@@ -1,23 +1,26 @@
 from django.db import models
 
+
 class Customer(models.Model):
     customer_name = models.CharField(max_length=500)
-    father_name = models.CharField(max_length=500)
-    customer_profile = models.CharField(max_length=500)
-    loan_account_number = models.CharField(max_length=500)
+    father_name = models.CharField(max_length=500, null=True)
+    customer_profile = models.CharField(max_length=500, null=True)
+    loan_account_number = models.CharField(max_length=500, null=True)
+
 
 class Branch(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    zone_name = models.CharField(max_length=500)
-    region_name = models.CharField(max_length=500)
-    area_name = models.CharField(max_length=500)
-    branch_name = models.CharField(max_length=500)
-    branch_code = models.CharField(max_length=500)
+    zone_name = models.CharField(max_length=500, null=True)
+    region_name = models.CharField(max_length=500, null=True)
+    area_name = models.CharField(max_length=500, null=True)
+    branch_name = models.CharField(max_length=500, null=True)
+    branch_code = models.CharField(max_length=500, null=True)
+
 
 class CustomerHomeAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    address1 = models.CharField(max_length=500)
-    address2 = models.CharField(max_length=500)
-    address3 = models.CharField(max_length=500)
-    landmark = models.CharField(max_length=500)
-    pincode = models.CharField(max_length=500)
+    address1 = models.CharField(max_length=500, null=True)
+    address2 = models.CharField(max_length=500, null=True)
+    address3 = models.CharField(max_length=500, null=True)
+    landmark = models.CharField(max_length=500, null=True)
+    pincode = models.CharField(max_length=500, null=True)
--- userapp/views.py
+++ userapp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from userapp.models import Customer
+from userapp.models import Customer, Branch, CustomerHomeAddress
 from django.views.decorators.csrf import csrf_exempt
 import openpyxl
 import json
@@ -9,14 +9,25 @@ import json
 def index(request):
     if request.method == "GET":
         try:
-            customer = Customer.objects.all()
-            # response = customer
-            response = "hi"
+            customer_data = []
+            customers = Customer.objects.all()
+            customers_list = list(customers.values())
+            for customer in customers_list:
+                customer_id = customer["id"]
+                branch_customer = Branch.objects.filter(customer=customer_id)
+                customer["branch"] = list(branch_customer.values())
+                address_customer = CustomerHomeAddress.objects.filter(
+                    customer=customer_id
+                )
+                customer["address"] = list(address_customer.values())
+                customer_data.append(customer)
+
+            response = {"customers": customer_data}
         except:
             response = json.dumps(
                 [{"Error occurred! Please try again after adding customers"}]
             )
-    return HttpResponse(response, content_type="text/json")
+    return render(request, "customers/list_customers.html", response)
 
 
 @csrf_exempt
@@ -33,10 +44,10 @@ def import_data(request):
                 # getting default sheet
                 worksheet = wb.active
 
-                # excel_data = list()
                 # iterating over the rows and
                 # getting value from each cell in row
                 worksheet_data = worksheet.iter_rows()
+
                 # skip the first row
                 next(worksheet_data)
                 for row in worksheet_data:
@@ -47,9 +58,9 @@ def import_data(request):
                         customer_profile=row[2].value,
                         loan_account_number=row[3].value,
                     )
-                    customer.save()
+                    customer_obj.save()
                     branch = Branch(
-                        customer=customer_obj.id,
+                        customer=Customer.objects.get(id=customer_obj.id),
                         zone_name=row[4].value,
                         region_name=row[5].value,
                         area_name=row[6].value,
@@ -58,7 +69,7 @@ def import_data(request):
                     )
                     branch.save()
                     address = CustomerHomeAddress(
-                        customer=customer_obj.id,
+                        customer=Customer.objects.get(id=customer_obj.id),
                         pincode=row[9].value,
                         landmark=row[10].value,
                         address1=row[11].value,
@@ -67,14 +78,11 @@ def import_data(request):
                     )
                     address.save()
 
-                    # for cell in row:
-                    #     row_data.append(str(cell.value))
-                    # excel_data.append(row_data)
-
-                return HttpResponse(excel_data, content_type="text/json")
+                response = redirect("/")
+                return response
             else:
                 return HttpResponse(
-                    "File type error! Please upload file in .xlsx format",
+                    "File type error! Please upload file in .xlsx format!",
                     content_type="text/json",
                 )
         except Exception as e:
